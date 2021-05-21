443df456d0d672f22c6be5b9a71c012fb99f5757
Satyaki Chatterjee <satyaki.chatterjee@quantuminventions.com>
2021-05-21 19:09:39 +0530

    feat: Added sidebar componet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Fragment } from "react";
 import { Provider } from "react-redux";
+import { Route, Router } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import { createStore } from "redux";
 import reducers from "./reducers";
 import generateFileSystem from "./utils/defaultFileSystem";
 import "./App.css";
+import styled from "styled-components";
+import Sidebar from "./components/Sidebar";
 
 const store = createStore(reducers, {
   fileSystem:
@@ -15,9 +19,31 @@ const store = createStore(reducers, {
 function App() {
   return (
     <Provider store={store}>
-      Setting up
+        <BrowserRouter>
+          <Fragment>
+            <Sidebar></Sidebar>
+          </Fragment>
+        </BrowserRouter>
     </Provider>
   );
 }
+
+const Container = styled.div`
+  padding: 41px;
+  margin-left: 320px;
+  transition: margin-left 250ms ease-in;
+  @media screen and (max-width: 768px) {
+    margin-left: 0px;
+    padding: 55px 15px 15px 15px;
+  }
+`;
+
+const TopBar = styled.div`
+  display: flex;
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+
 
 export default App;
