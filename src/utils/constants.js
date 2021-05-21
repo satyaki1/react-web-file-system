1a6f26d0e30157ba7807194a026fd9757d4e951d
Satyaki Chatterjee <satyaki.chatterjee@quantuminventions.com>
2021-05-21 14:58:20 +0530

    feat: Specified Redux constants

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+// Types of items to showcase
+const FILE = 'FILE';
+const FOLDER = 'FOLDER';
+
+// Types of action names
+const ADD_ENTRY = 'ADD_ENTRY';
+const DELETE_ENTRY = 'DELETE_ENTRY';
+
+// Types of Search
+const LOCAL = 'LOCAL';
+const GLOBAL = 'GLOBAL';
+
+export { FILE, FOLDER, ADD_ENTRY, DELETE_ENTRY, LOCAL, GLOBAL };
