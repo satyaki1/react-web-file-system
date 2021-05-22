82c5be61c9ac46e8a0078f0dc5910f26dffe34f9
satyaki1 <satyaki1@users.noreply.github.com>
2021-05-22 17:10:23 +0000

    Deploying to gh-pages from @ satyaki1/react-web-file-system@059914cfaf7578a4b8430e4e0a3932f0df4f36f3 🚀

--- src/components/Sidebar/Collapse.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import { CollapseContainer } from "./styles";
-
-const Collapse = (props) => {
-  const [visible, handleVisible] = useState(false);
-  return <CollapseContainer>{props.children(visible, () => handleVisible(!visible))}</CollapseContainer>;
-};
-
-export default Collapse;
--- src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from "redux";
-import fileSystem from "./fileSystemReducer.js";
-
-export default combineReducers({ fileSystem });
